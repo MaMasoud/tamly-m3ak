7655d6367caf9e75bffbed78d9947a3f7a7cc081
MaMasoud <hpdhooda@gmail.com>
2024-06-30 14:21:43 +0300

    push starting

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+        <div className="logo">تملي معاك</div>
+        <nav>
+            <ul>
+                <li><a href="#">تسجيل الدخول</a></li>
+                <li><a href="#">انشاء حساب</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
